2ad4637a70da80954116e67b6da22006a569e361
Myles Bennett <myles.bennett@hotmail.com>
2019-03-29 15:50:01 -0400

    completed exercise

---
+++ exercise.js
@@ -0,0 +1,5 @@
+const tasks = ["laundry", "shovel driveway", "grocery shopping", "check email", "daydream", "phone parents", "fill prescription", "exercise", "take out recycling", "check the weather"];
+for(let i = 0; i < tasks.length; i++) {
+  console.log("Sunday To Do List:");
+  console.log(`${i + 1}: ${tasks[i]}`);
+}
